023c38776e54f3ee3f4e68e78d3ba99209292db7
DJ Knox <dhknox@gmail.com>
2019-07-29 11:46:46 -0400

    set up rankings page

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('rankings/', views.rankings, name='rankings'),
 ]
--- core/views.py
+++ core/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def rankings(request):
+    return render(request, 'core/rankings.html')
