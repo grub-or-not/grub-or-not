24a0a2d6209bc6628325a44f9ef1926e357a6005
Daniel E Calderon <Daniel.Calderon06@gmail.com>
2019-07-31 16:25:50 -0400

    Add line chart

--- core/views.py
+++ core/views.py
@@ -23,47 +23,6 @@ def user_profile(request):
     }
     return render(request, 'core/user_profile.html', context)
 
-<<<<<<< HEAD
-# Below is new code for charts
-
-from django.contrib.auth import get_user_model
-from django.http import JsonResponse
-from django.views.generic import View
-from rest_framework.views import APIView
-from rest_framework.response import Response
-
-
-User = get_user_model()
-class HomeView(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'charts.html', {"customers": 10})
-
-
-
-def get_data(request, *args, **kwargs):
-    data = {
-        "sales": 100,
-        "customers": 10,
-    }
-    return JsonResponse(data) # http response
-
-
-class ChartData(APIView):
-    authentication_classes = []
-    permission_classes = []
-
-    def get(self, request, format=None):
-        qs_count = User.objects.all().count()
-        labels = ["Users", "Blue", "Yellow", "Green", "Purple", "Orange"]
-        default_items = [qs_count, 23, 2, 3, 12, 2]
-        data = {
-                "labels": labels,
-                "default": default_items,
-        }
-        return Response(data)
-
-=======
-
 def yelp_search(request, term, longitude, latitude, limit):
     api_key='LXc_1CXYWbpCcRrhXYCQ8UVdROphcKPdlDoR-EC9GGadzfBh-iTLBpqmhNPCI3_on1IroKPRcFNWffn3Y3orgE50ho4k0j-VABxhBrJgPrsfn7RssZavS4-S47k9XXYx'
     headers = {'Authorization': f'Bearer {api_key}'}
@@ -76,4 +35,3 @@ def yelp_search(request, term, longitude, latitude, limit):
     }
     req=requests.get(url, params=params, headers=headers)
     return JsonResponse(json.loads(req.text))
->>>>>>> master
