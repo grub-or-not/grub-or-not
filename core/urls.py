9f47505848e9fbe6c759d8613aa7adbec313a741
Meagan-M <meaganmakinson80@gmail.com>
2019-08-10 17:53:24 -0400

    set up url, view and html template for about page

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('browse/', views.browse, name='browse'),
+    path('about/', views.about, name='about'),
     path('yelp/<term>/<longitude>/<latitude>/<limit>/', views.yelp_search, name="yelp_search"),
 ]
